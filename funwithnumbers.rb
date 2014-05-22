a0b4793866aff7174679145f8a3c5a579682a6bf
Rcamach <rudycamacho@Rudys-MacBook-Pro.local>
2014-05-22 16:03:29 -0600

    funwithnumbers

---
+++ funwithnumbers.rb
@@ -0,0 +1,13 @@
+puts "Give me a number" + '.'
+
+number1 = gets.chomp.to_i
+
+puts "Give me another number" + '.'
+
+number2 = gets.chomp.to_i
+
+
+result = number1 + number2
+
+
+puts "#{number1} + #{number2} = #{result}"
